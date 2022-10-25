00a671e8122da41aaef3bb113f79cbf0d319171e
Sid-Pat <siddhantujjain123@gmail.com>
2022-10-26 04:23:11 +0530

    added string cpp file

---
+++ concatenate.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+    string s = "Hello ";
+    string r = "World ";
+    string res = s+r;
+    cout<<res<<endl;
+return 0; 
+}
